4ab36fbd32b60f82ee2f198f07948eaabe0c0208
dlustre <dennis.lustre@gmail.com>
2024-12-01 10:35:38 -0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,14 +2,15 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    command = input()
+        command = input()
 
-    match command.split():
-        case [command, *args]:
-            print(f"{command}: command not found")
+        match command.split():
+            case [command, *args]:
+                print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
